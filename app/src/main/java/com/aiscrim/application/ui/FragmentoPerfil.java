8caed5682c866a43327e5bb0a94f8cab04c98e57
Nacho Crespo <igancio.crespo.martinez@gmail.com>
2016-05-27 20:04:35 +0200

    información usuario evitable

--- app/src/main/java/com/aiscrim/application/ui/FragmentoPerfil.java
+++ app/src/main/java/com/aiscrim/application/ui/FragmentoPerfil.java
@@ -3,9 +3,13 @@ package com.aiscrim.application.ui;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,8 +24,9 @@ import org.w3c.dom.Text;
  */
 public class FragmentoPerfil extends Fragment {
     ImageButton editar;
+    ImageButton guardar;
     CardView card;
-    TextView nombreApellidos;
+    EditText nombreApellidos;
     TextView mail;
     TextView tlf;
 
@@ -33,7 +38,7 @@ public class FragmentoPerfil extends Fragment {
                              Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragmento_perfil, container, false);
 
-        nombreApellidos = (TextView) view.findViewById(R.id.texto_nombre);
+        nombreApellidos = (EditText) view.findViewById(R.id.texto_nombre);
         mail = (TextView) view.findViewById(R.id.texto_email);
         tlf = (TextView) view.findViewById(R.id.texto_telefono);
 
@@ -45,7 +50,23 @@ public class FragmentoPerfil extends Fragment {
         editar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast.makeText(v.getContext(), "Pulsado boton editar", Toast.LENGTH_SHORT).show();
+                nombreApellidos.setEnabled(true);
+                mail.setEnabled(true);
+                tlf.setEnabled(true);
+                guardar.setVisibility(View.VISIBLE);
+                editar.setVisibility(View.INVISIBLE);
+            }
+        });
 
+        guardar = (ImageButton) view.findViewById(R.id.boton_guardar);
+        guardar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Pulsado boton guardar", Toast.LENGTH_SHORT).show();
+                nombreApellidos.setEnabled(false);
+                mail.setEnabled(false);
+                tlf.setEnabled(false);
+                guardar.setVisibility(View.INVISIBLE);
+                editar.setVisibility(View.VISIBLE);
             }
         });
 
